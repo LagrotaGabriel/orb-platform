43d3e9f6e0c765501e33f6f664310ef18e9b4170
Gabriel Lagrota <95034068+LagrotaGabriel@users.noreply.github.com>
2025-04-11 23:35:37 -0300

    feat: Enhance CupResponse to include last cup metrics data

--- backend/src/main/java/orb/com/backend/modules/cup/models/dto/CupResponse.java
+++ backend/src/main/java/orb/com/backend/modules/cup/models/dto/CupResponse.java
@@ -3,6 +3,7 @@ package orb.com.backend.modules.cup.models.dto;
 import orb.com.backend.modules.cup.models.entity.CupEntity;
 import orb.com.backend.modules.cup.models.enums.CupColorEnum;
 import orb.com.backend.modules.cup.models.enums.CupModelEnum;
+import orb.com.backend.modules.metrics.models.dto.response.CupMetricsResponse;
 
 import java.time.LocalDateTime;
 import java.util.UUID;
@@ -13,7 +14,8 @@ public record CupResponse(
         LocalDateTime lastSyncDateTime,
         boolean isActivated,
         CupModelEnum model,
-        CupColorEnum color
+        CupColorEnum color,
+        CupMetricsResponse lastMetric
 ) {
     public static CupResponse createFromEntity(CupEntity cupEntity) {
         return new CupResponse(
@@ -22,7 +24,10 @@ public record CupResponse(
                 cupEntity.getLastSyncDateTime(),
                 cupEntity.getIsActivated(),
                 cupEntity.getModel(),
-                cupEntity.getColor()
+                cupEntity.getColor(),
+                cupEntity.getMetrics().isEmpty()
+                        ? null
+                        : CupMetricsResponse.createFromEntity(cupEntity.getMetrics().get(cupEntity.getMetrics().size() - 1))
         );
     }
 }

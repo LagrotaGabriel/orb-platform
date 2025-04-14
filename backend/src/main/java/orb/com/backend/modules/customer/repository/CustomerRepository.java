230310c97381ab132e1176c485ae3a15737ecb73
Gabriel Lagrota <95034068+LagrotaGabriel@users.noreply.github.com>
2025-04-13 22:54:03 -0300

    feat: Add method to find CustomerEntity by email in CustomerRepository

--- backend/src/main/java/orb/com/backend/modules/customer/repository/CustomerRepository.java
+++ backend/src/main/java/orb/com/backend/modules/customer/repository/CustomerRepository.java
@@ -4,10 +4,13 @@ import orb.com.backend.modules.customer.models.entity.CustomerEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 @Repository
 public interface CustomerRepository extends JpaRepository<CustomerEntity, UUID> {
 
     boolean existsByEmail(String email);
+
+    Optional<CustomerEntity> findByEmail(String email);
 }

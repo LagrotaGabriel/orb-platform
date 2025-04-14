7b88f98d89368addb06f6e1a6521a661b15141be
Gabriel Lagrota <95034068+LagrotaGabriel@users.noreply.github.com>
2025-04-13 22:24:43 -0300

    fix: Change response status to CONFLICT for DuplicateResourceException

--- backend/src/main/java/orb/com/backend/exceptions/models/DuplicateResourceException.java
+++ backend/src/main/java/orb/com/backend/exceptions/models/DuplicateResourceException.java
@@ -3,7 +3,7 @@ package orb.com.backend.exceptions.models;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-@ResponseStatus(HttpStatus.BAD_REQUEST)
+@ResponseStatus(HttpStatus.CONFLICT)
 public class DuplicateResourceException extends RuntimeException{
 
     public DuplicateResourceException(String message) {

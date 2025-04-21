e53f169352952ad541fce4c3342e3a95eb040b53
Gabriel Lagrota <95034068+LagrotaGabriel@users.noreply.github.com>
2025-04-21 03:40:52 -0300

    refactor: Replace DataIntegrityViolationException handling with RuntimeException in CreateCupServiceImpl

--- backend/src/main/java/orb/com/backend/modules/cup/actions/create/service/impl/CreateCupServiceImpl.java
+++ backend/src/main/java/orb/com/backend/modules/cup/actions/create/service/impl/CreateCupServiceImpl.java
@@ -4,11 +4,10 @@ package orb.com.backend.modules.cup.actions.create.service.impl;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import orb.com.backend.modules.cup.actions.create.dto.CreateCupRequest;
-import orb.com.backend.modules.cup.models.dto.CupResponse;
 import orb.com.backend.modules.cup.actions.create.service.CreateCupService;
+import orb.com.backend.modules.cup.models.dto.CupResponse;
 import orb.com.backend.modules.cup.models.entity.CupEntity;
 import orb.com.backend.modules.cup.repository.CupRepository;
-import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -31,12 +30,9 @@ public class CreateCupServiceImpl implements CreateCupService {
 
             return CupResponse.createFromEntity(savedCup);
 
-        } catch (DataIntegrityViolationException dataIntegrityViolationException) {
-            log.error("Data integrity violation occurred while creating a new cup: {}", dataIntegrityViolationException.getMessage());
-            throw new InternalError("An error occurred while creating a new cup");
         } catch (Exception exception) {
             log.error("An unexpected error occurred while creating a new cup: {}", exception.getMessage());
-            throw new InternalError("An unexpected error occurred while creating a new cup");
+            throw new RuntimeException("An unexpected error occurred while creating a new cup", exception);
         }
     }
 }
